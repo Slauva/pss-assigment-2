3458fab3dec7f20c55ab5a5b19ec49dcf71a6a05
Slauva <koshman2015@list.ru>
2021-03-05 18:30:42 +0300

    for BackUp

---
+++ src/Admin.cpp
@@ -0,0 +1,10 @@
+#include "../include/profile/Admin.h"
+
+void Admin::red_button() {
+}
+void Admin::change_of_government() {}
+void Admin::shadowing(Asset) {}
+void Admin::shadowing(Asset*) {}
+void Admin::drop(std::string) {}
+void Admin::move_out(Asset) {}
+void Admin::move_out(Asset*) {}
